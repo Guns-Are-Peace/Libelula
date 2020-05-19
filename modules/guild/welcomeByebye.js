30ec2e69e4418dbf991705f9eb74616e5ed08a4a
SCP-George <georgesantos11@protonmail.com>
2020-05-19 06:26:33 -0300

    Broken commit correction

--- modules/guild/welcomeByebye.js
+++ modules/guild/welcomeByebye.js
@@ -1,50 +1,62 @@
-const { Command } = include('bucket/index')
+const { Module } = include("bucket");
+const moment     = require('moment');
 
-module.exports = class Ban extends Command {
+module.exports = class botGuild extends Module {
     constructor(...args) {
         super(...args, {
-            name: 'ban',
-            group: 'moderation',
-            aliases: [],
-            cooldown: 5,
-            description: 'Ban a user',
-            options: {
-                guildOnly: true,
-                localeKey: "commands",
-                permissions: ['banMembers']
-            },
-            usage: [{
-                name: 'member',
-                displayName: 'id/mention/username',
-                type: 'member',
-                optional: false
-            },
-            {
-                name: 'reason',
-                displayName: 'reason',
-                type: 'string',
-                max: 512,
-                last: true,
-                optional: true
-            }]
-        })
-    }
-
-    async handle({ args, msg, client }, responder) {
-        const member = args.member[0];
-        const reason = args.reason || responder.t('reasonNotSet');
-        const userBot = msg.guild.members.get(client.user.id);
-
-        const punisherhighestRole = member.highestRole.position < msg.member.highestRole.position
-        const bothighestRole = member.highestRole.position < userBot.highestRole.position
-
-        if (!punisherhighestRole)
-            return responder.error(responder.t('{{ban.equalOrHigher}}'));
-        if (!bothighestRole)
-            return responder.error('{{ban.botLowRole}}');
+            name: "welcommer",
+            events: {
+                guildMemberAdd: 'onJoin',
+                guildMemberDelete: 'onLeave'
+            }
+        });
+    };
+    init() {
+        return this.db = this._client.plugins.get('store');
+    };
+    get tags() {
+        'user',
+        'userName',
+        'guild',
+        'date',
+        'members',
+        'humans'
+    };
+    async onJoin(guild, member) {
+        const gData = await this.db.fetchGuild(guild.id);
+        if (!gData.modules.welcome.channel || !gData.modules.welcome) return;
         
-        member.ban(null, reason)
-            .then(() => responder.success(responder.t('{{ban.success}}')))
-            .catch(() => responder.error('ban.unexpectedRejection'));
+        let chan = gData.modules.welcome.channel;
+        let msg  = gData.modules.welcome.text;
+
+        this.send(chan, this.i18n.shift(msg, {
+            user: member.mention,
+            guild: guild.name,
+            userName: member.user.username,
+            date: moment(moment()).format('lll'),
+            members: guild.members.size,
+            humans: guild.members.filter(m => m.bot).length
+        }))
+        .catch(e => this.logger.error('Error while sending welcome message', e));
+        typeof gData.modules.autorole == 'object'
+        && member.addRole(gData.modules.autorole, "Autorole system - Jasper")
+        .catch(e => this.logger.error('Error while setting autorole.', e));
+    };
+    async onLeave(guild) {
+        const gData = await this.db.fetchGuild(guild.id);
+        if (!gData.modules.welcome.channel || !gData.modules.welcome) return;
+        
+        let chan = gData.modules.welcome.channel;
+        let msg  = gData.modules.welcome.text;
+
+        this.send(chan, this.i18n.shift(msg, {
+            user: member.mention,
+            guild: guild.name,
+            userName: member.user.username,
+            date: moment(moment()).format('lll'),
+            members: guild.members.size,
+            humans: guild.members.filter(m => m.bot).length
+        }))
+        .catch(e => this.logger.error('Error while sending welcome message', e));
     };
 };
